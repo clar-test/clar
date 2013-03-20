7ee6807399420509d0c8f2d651c0d2c9e57f05e6
Russell Belfer <rb@github.com>
2013-03-20 10:57:56 -0700

    Added annotated versions of clar_assert_equal...
    This adds cl_assert_equal_s_ and cl_assert_equal_i_ which allow
    for an extra annotation parameter when using these assertions.
    I find that I sometimes need to append a little extra data to a
    series of numeric assertions to understand exactly where the error
    originated.

--- clar.h
+++ clar.h
@@ -58,8 +58,13 @@ void cl_fixture_cleanup(const char *fixture_name);
  * Typed assertion macros
  */
 #define cl_assert_equal_s(s1,s2) clar__assert_equal_s((s1),(s2),__FILE__,__LINE__,"String mismatch: " #s1 " != " #s2, 1)
+#define cl_assert_equal_s_(s1,s2,note) clar__assert_equal_s((s1),(s2),__FILE__,__LINE__,"String mismatch: " #s1 " != " #s2 " (" #note ")", 1)
+
 #define cl_assert_equal_i(i1,i2) clar__assert_equal_i((i1),(i2),__FILE__,__LINE__,#i1 " != " #i2, 1)
+#define cl_assert_equal_i_(i1,i2,note) clar__assert_equal_i((i1),(i2),__FILE__,__LINE__,#i1 " != " #i2 " (" #note ")", 1)
+
 #define cl_assert_equal_b(b1,b2) clar__assert_equal_i(!!(b1),!!(b2),__FILE__,__LINE__,#b1 " != " #b2, 1)
+
 #define cl_assert_equal_p(p1,p2) cl_assert((p1) == (p2))
 
 void clar__fail(
